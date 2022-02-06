934f310494ca5d42806f01659664c1b6400778a4
salahashraf253 <salahashraf924@gmail.com>
2022-02-06 19:44:10 +0200

    add Employee classes

---
+++ Gym System/Person Classes/Gym Employees/Administrator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_System.Person_Classes.Gym_Employees
+{
+    internal class Administrator
+    {
+    }
+}
